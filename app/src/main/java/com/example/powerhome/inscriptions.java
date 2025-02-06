4d4460dd5da0fb8348c4edeee7fd44cde799361b
RaphaelSpencerDaGraca <oversquad04@gmail.com>
2025-02-06 16:49:22 +0100

    Avant note top

--- app/src/main/java/com/example/powerhome/inscriptions.java
+++ app/src/main/java/com/example/powerhome/inscriptions.java
@@ -1,6 +1,8 @@
 package com.example.powerhome;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,10 @@ public class inscriptions extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Spinner spinnerPrefix = findViewById(R.id.spinner_prefix);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.phone_prefixes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPrefix.setAdapter(adapter);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inscriptions);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
